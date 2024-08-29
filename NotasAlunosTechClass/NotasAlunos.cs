e8d387d0becfac3298c559b60e2cffeb9a76f4da
Cristian Serena <devcristian02@gmail.com>
2024-08-28 21:40:16 -0300

    POO notas alunos

---
+++ NotasAlunosTechClass/NotasAlunos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NotasAlunosTechClass
+{
+    public class NotasAlunos
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+        public double PontosFaltantes = 60;
+        public double Resultado;
+
+        public double CalculaMedia()
+        {
+            
+            Resultado = (Nota1 * 30 / 100) + (Nota2 * 35 / 100) + (Nota3 * 35 / 100);
+
+            if (Resultado >= PontosFaltantes)
+            {
+                Console.WriteLine($"Parabéns, sua nota foi: {Resultado:F2}. Você está APROVADO.");
+            }
+            else
+            {
+                Console.WriteLine($"Oh não, não desanime. Sua nota foi: {Resultado:F2}. Você está REPROVADO.");
+            }
+
+            return Resultado;
+        }
+
+        public double CalculaPontosFaltantes()
+        {
+           
+            if (Resultado < PontosFaltantes)
+            {
+                double pontos = PontosFaltantes - Resultado;
+                Console.WriteLine($"Você precisa de mais {pontos:F2} pontos para ser aprovado.");
+                return pontos;
+            }
+            else
+            {
+                
+                Console.WriteLine("Nenhum ponto faltante, você está aprovado.");
+                return 0;
+            }
+        }
+    }
+}
+
+
---
+++ NotasAlunosTechClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NotasAlunosTechClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotasAlunos notasAlunos = new NotasAlunos();
+            string sep = new string('-', 50);
+            Console.WriteLine("Programa para Calcular a média Ponderada");
+            Console.WriteLine(sep);
+
+            Console.Write("Digite o Nome do aluno: ");
+            notasAlunos.Nome = Console.ReadLine();
+
+            Console.Write("Digite 1° Nota: ");
+            notasAlunos.Nota1 = double.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite 2° Nota: ");
+            notasAlunos.Nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite 3° Nota: ");
+            notasAlunos.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(sep);
+
+            Console.WriteLine($"Sua media foi: {notasAlunos.CalculaMedia()}");
+            Console.WriteLine($"Faltaram: {notasAlunos.CalculaPontosFaltantes()}");
+
+
+            Console.WriteLine(sep);
+        }
+    }
+}
