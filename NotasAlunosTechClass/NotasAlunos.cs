f91f181abc2f5a44775426ab6844e6353af29c49
Cristian Serena <devcristian02@gmail.com>
2024-09-04 23:10:21 -0300

    Simplificando gets e sets

--- NotasAlunosTechClass/NotasAlunos.cs
+++ NotasAlunosTechClass/NotasAlunos.cs
@@ -1,44 +1,12 @@
 ﻿public class NotasAlunos
 {
-    private string _nome;
-    private double _nota1, _nota2, _nota3;
-    private double _pontosFaltantes = 60;
-    private double _media;
+    public string Nome { get; set; }
+    public double Nota1 { get; set; }
+    public double Nota2 { get; set; }
+    public double Nota3 { get; set; }
+    public double Media;
+    private const double PontosFaltantes = 60;
 
-    
-    public string Nome
-    {
-        get { return _nome; }
-        set { _nome = value; }
-    }
-
-   
-    public double Nota1
-    {
-        get { return _nota1; }
-        set { _nota1 = value; }
-    }
-
-    public double Nota2
-    {
-        get { return _nota2; }
-        set { _nota2 = value; }
-    }
-
-    public double Nota3
-    {
-        get { return _nota3; }
-        set { _nota3 = value; }
-    }
-
-    
-    public double Media
-    {
-        get { return _media; }
-        set { _media = value; } 
-    }
-
-   
     public NotasAlunos(string nome, double nota1, double nota2, double nota3)
     {
         Nome = nome;
@@ -47,7 +15,6 @@
         Nota3 = nota3;
     }
 
- 
     public double CalculaMediaPonderada()
     {
         Media = (Nota1 * 30/100) + (Nota2 * 35/100) + (Nota3 * 35/100);
